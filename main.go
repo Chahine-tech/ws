e1a23485fe6e03d853d7ca31f97e2da5cf73e3ed
syd2 <king.jr3737@gmail.com>
2022-12-12 15:40:08 +0100

    commit

--- main.go
+++ main.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-
 	"github.com/Chahine-tech/ws/handlers"
 	"github.com/Chahine-tech/ws/server"
 	"github.com/go-chi/chi/v5"
@@ -14,7 +11,7 @@ func main() {
 	r := chi.NewRouter()
 	s := server.NewServer()
 	r.Use(middleware.Logger)
+	s.Router.Mount("/", r)
 	s.Router.Get("/hello", handlers.HelloHandler())
-	fmt.Println("Serving on port 9000....")
-	http.ListenAndServe(":9000", r)
+	s.RunServer()
 }
